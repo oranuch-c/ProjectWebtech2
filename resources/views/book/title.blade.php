7d371c7ba8cdd0a8c3051c142385a8a6ee292109
thanatchaporn sri-art <thanatchaporn.s@ku.th>
2017-05-09 09:08:31 +0700

    edit page search book

--- resources/views/book/title.blade.php
+++ resources/views/book/title.blade.php
@@ -1,12 +1,16 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+    @include('layouts._head')
+<body>
+    @include('layouts._nav')
 
-@section('content')
+
 <h1 class="title">All Books</h1>
 <div class="row">
 
-   <div class="col-md-12">
-        <div class="panel panel-default" id="vue-app">
-            <div class="panel-heading">
+   <div class="col-lg-12 margin-tb">
+        <div class="span20 " id="vue-app">
+            <div class="panel-heading ">
                 Data Render By Blade
             </div>
             <div class="panel-body">
@@ -17,31 +21,29 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Price</th>
-                        <th>Author</th>
-                        <th>Year</th>
-                        <th>Page</th>
-                        <th>Publish Company</th>
-                        <th>Image</th>
+                        <th >ID</th>
+                        <th >Name</th>
+                        <th >Status</th>
+                        <th >Price</th>
+                        <th >Author</th>
+                        <th >Year</th>
+                        <th >Page</th>
+                        <th >Publish Company</th>
+                        <th >Image</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($data as $d)
                     <tr>
                         <td>{{ $d->id }}</td>
-                        <td>
-                            {{ $d->name }}
-                        </td>
+                        <td>{{ $d->name }}</td>
                         <td>{{ $d->status }}</td>
                         <td>{{ $d->price }}</td>
                         <td>{{ $d->author }}</td>
                         <td>{{ $d->year }}</td>
                         <td>{{ $d->pageSize }}</td>
                         <td>{{ $d->publishComp }}</td>
-                        <td style="width: 10%"><img style="width: 100%" src= "{{ asset('img/'.$d->img) }}"></td>
+                        <td style="width: 5%"><img style="width: 100%" src= "{{ asset('img/'.$d->img) }}"></td>
                     </tr>
                     @endforeach
                 </tbody>
@@ -50,62 +52,16 @@
     </div>
 
 </div>
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default" id="json-beautifier">
-            <div class="panel-heading">
-                JSON Response
-            </div>
-            <div class="panel-body">
-                <pre>@{{json}}</pre>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-6">
-        <div class="panel panel-default" id="vue-app-titles">
-            <div class="panel-heading">
-                Data Render By VueJS
-            </div>
-            <div class="panel-body">
-                Success <label class="label label-success">@{{ success }}</label>
-            </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Price</th>
-                        <th>Author</th>
-                        <th>Year</th>
-                        <th>Page</th>
-                        <th>Publish Company</th>
-
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="d in data">
-                        <td>@{{ d.id }}</td>
-                        <td>@{{ d.name }}</td>
-                        <td>@{{ d.status }}</td>
-                        <td>@{{ d.price }}</td>
-                        <td>@{{ d.author }}</td>
-                        <td>@{{ d.year }}</td>
-                        <td>@{{ d.pageSize }}</td>
-                        <td>@{{ d.publishComp }}</td>
 
 
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+<!-- MainBody End ============================= -->
+<!-- Footer ================================================================== -->
+    @include('layouts._footer')
+<!-- Placed at the end of the document so the pages load faster ============================================= -->
+    @include('layouts._script')
 
-</div>
-
-@endsection
+</body>
+</html>
 
 @section('script')
 <script>
--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Sidebar ================================================== -->
-	<div id="sidebar" class="span3">
+<!-- 	<div id="sidebar" class="span3">
 		<div class="well well-small"><a id="myCart" href="product_summary.html"><img src="themes/images/ico-cart.png" alt="cart">3 Items in your cart  <span class="badge badge-warning pull-right">$155.00</span></a></div>
 		<ul id="sideManu" class="nav nav-tabs nav-stacked">
 			<li class="subMenu open"><a> ELECTRONICS [230]</a>
@@ -38,4 +38,4 @@
 			<li><a href="products.html">BOOKS & ENTERTAINMENTS [14]</a></li>
 		</ul>
 	
-	</div>
+	</div> -->
